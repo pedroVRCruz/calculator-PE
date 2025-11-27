7fd843af77dcc3348d918a7706608f258c3fd219
Pedro Cruz <168193494+pedroVRCruz@users.noreply.github.com>
2025-11-27 01:46:38 -0300

    feat: basic functions

--- src/calculadora.c
+++ src/calculadora.c
@@ -3,8 +3,7 @@
  Name        : calculadora.c
  Author      : Pedro, Cassia e João
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Calculadora simples em C
  ============================================================================
  */
 
@@ -31,8 +30,9 @@ void dividir(int a, int b) {
     printf("A divisão é: %d\n", a / b);
 }
 
-void seed(int seed, int n) {
-    srand(seed);
+void seed(int seed_value, int n) {
+    srand(seed_value);
+
     printf("Número gerado: ");
     printf("%d", rand() % 9 + 1);
 
@@ -42,9 +42,9 @@ void seed(int seed, int n) {
     printf("\n");
 }
 
-void soma_por_digitos(a,b) {
-    int a_long = (long long)a;
-    int b_long = (long long)b;
+void soma_por_digitos(int a, int b) {
+    long long a_long = (long long)a;
+    long long b_long = (long long)b;
     int resultado[200];
     int i = 0;
     int sobra = 0;
@@ -58,41 +58,85 @@ void soma_por_digitos(a,b) {
         a_long /= 10;
         b_long /= 10;
     }
+
     printf("Resultado: ");
     for (int j = i - 1; j >= 0; j--) {
         printf("%d", resultado[j]);
     }
-
-    ("\n");
+    printf("\n");
 }
 
 int main() {
     int num_usuario;
     int a, b;
+    int continuar = 1;
 
-    printf("Escolha uma opção:\n");
-    printf("1 - Soma\n");
-    printf("2 - Subtração\n");
-    printf("3 - Multiplicação\n");
-    printf("4 - Divisão\n");
-    printf("5 - Geração de número aleatório com seed\n");
-    printf("6 - Soma digito por digito\n");
-    printf("7 - Sair\n");
-    printf("Opção: ");
-    scanf("%d", &num_usuario);
+    while (continuar) {
+        printf("\nEscolha uma opção:\n");
+        printf("1 - Soma\n");
+        printf("2 - Subtração\n");
+        printf("3 - Multiplicação\n");
+        printf("4 - Divisão\n");
+        printf("5 - Geração de número aleatório com seed\n");
+        printf("6 - Soma dígito por dígito\n");
+        printf("7 - Sair\n");
+        printf("Opção: ");
 
-    printf("Digite dois números: \n");
-    scanf("%d %d", &a, &b);
+        if (scanf("%d", &num_usuario) != 1) {
+            // Leitura inválida: limpa buffer e continua
+            printf("Entrada inválida.\n");
+            // Consome caractere inválido
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            continue;
+        }
 
-    switch (num_usuario) {
-        case 1: somar(a, b); break;
-        case 2: subtrair(a, b); break;
-        case 3: multiplicar(a, b); break;
-        case 4: dividir(a, b); break;
-        case 5: seed(a, b); break;
-        case 6: soma_por_digitos(a, b); break;
-        case 7: printf("Saindo do programa.\n"); break;
-        default: printf("Opção inválida.\n"); break;
+        switch (num_usuario) {
+            case 1:
+                printf("Digite dois números: \n");
+                scanf("%d %d", &a, &b);
+                somar(a, b);
+                break;
+
+            case 2:
+                printf("Digite dois números: \n");
+                scanf("%d %d", &a, &b);
+                subtrair(a, b);
+                break;
+
+            case 3:
+                printf("Digite dois números: \n");
+                scanf("%d %d", &a, &b);
+                multiplicar(a, b);
+                break;
+
+            case 4:
+                printf("Digite dois números: \n");
+                scanf("%d %d", &a, &b);
+                dividir(a, b);
+                break;
+
+            case 5:
+                printf("Digite o seed e a quantidade de dígitos: \n");
+                scanf("%d %d", &a, &b);
+                seed(a, b);
+                break;
+
+            case 6:
+                printf("Digite dois números para somar dígito por dígito: \n");
+                scanf("%d %d", &a, &b);
+                soma_por_digitos(a, b);
+                break;
+
+            case 7:
+                printf("Saindo do programa.\n");
+                continuar = 0;
+                break;
+
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
     }
 
     return 0;
